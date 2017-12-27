6d088f09a0a57ab9d76b7a6d87cd107d3d03d8ba
Juan Diego <jdcm10@gmail.com>
2017-12-27 09:13:36 -0500

    definir 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-<h1>HOLA JAJAJAJAJJAJAJAJAJAJJAJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+  	<header class="text-center" name="home">
+		<div class="intro-text notfound">
+		    <h1><strong>Oooops.</strong></h1>
+		    <p>Parece que la página a la que quieres acceder no existe.</p>
+		    <p>Prueba con nuestro menu de arriba o vuelve a la página principal.</p> 
+		</div>
+	</header>
 
 @endsection
